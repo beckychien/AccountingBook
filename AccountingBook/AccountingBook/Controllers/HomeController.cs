519c7ade6bcbd3f8f693eaad2a619c0ac861fb4b
BeckyChien <becky6110@gmail.com>
2017-08-08 22:27:21 +0800

    MVC Session 1 HW

--- AccountingBook/AccountingBook/Controllers/HomeController.cs
+++ AccountingBook/AccountingBook/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AccountingBook.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +11,25 @@ namespace AccountingBook.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            List<AccountingBookViewModel> AcBookLists = new List<AccountingBookViewModel>();
+            AccountingBookViewModel ac1 = new AccountingBookViewModel();
+            ac1.Type = "支出";
+            ac1.DT = DateTime.Now.Date.AddDays(-2);
+            ac1.Cost = 1000;
+            AcBookLists.Add(ac1);
+
+            AccountingBookViewModel ac2 = new AccountingBookViewModel();
+            ac2.Type = "支出";
+            ac2.DT = DateTime.Now.Date.AddDays(-1);
+            ac2.Cost = 500;
+            AcBookLists.Add(ac2);
+
+            AccountingBookViewModel ac3 = new AccountingBookViewModel();
+            ac3.Type = "支出";
+            ac3.DT = DateTime.Now.Date;
+            ac3.Cost = 1300;
+            AcBookLists.Add(ac3);
+            return View(AcBookLists);
         }
 
         public ActionResult About()
@@ -25,6 +44,7 @@ namespace AccountingBook.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
-        }
+        }      
+
     }
 }
---
+++ AccountingBook/AccountingBook/Models/ViewModels/AccountingBookViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AccountingBook.Models.ViewModels
+{
+    public class AccountingBookViewModel
+    {
+        [DisplayName("類型")]
+        public string Type { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        [DisplayName("日期")]
+        public DateTime DT { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:#,##0.##}")]
+        [DisplayName("金額")]
+        public decimal Cost { get; set; }
+    }
+}
